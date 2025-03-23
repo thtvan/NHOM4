2fd80ab93a2ea7abf28eb10df03ab137349bb315
nguyen ho bao nghi <nghinhbk24406@st.uel.edu.vn>
2025-03-23 22:34:09 +0700

    them file ui

--- UI/main_UI.py
+++ UI/main_UI.py
@@ -1,6 +1,7 @@
-import sys
 from PyQt6 import QtWidgets
-from App_UI import Ui_MainWindow
+from NHOM4.UI.App_UI import Ui_MainWindow
+
+import sys
 
 class MainApp(QtWidgets.QMainWindow):
     def __init__(self):
@@ -8,26 +9,31 @@ class MainApp(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # nối chức năng
-        self.ui.pushButton.clicked.connect(self.switch_choose)
-        self.ui.btnLogIn.clicked.connect(self.switch_main)
-        self.ui.btnYes.clicked.connect(self.switch_signup)
-        self.ui.btnNo.clicked.connect(self.switch_login)
+        # Đặt kích thước cửa sổ nhỏ hơn
+        self.setFixedSize(906, 750)  # Kích thước cố định
 
-    def switch_choose(self):
-        #hỏi có tài khoản
+        # Kết nối các nút với chức năng tương ứng
+        self.ui.pushButton.clicked.connect(self.switch_to_askTK)  # Từ Logo -> AskTK
+        self.ui.btnYes.clicked.connect(self.switch_to_signup)     # Từ AskTK -> SignUp
+        self.ui.btnNo.clicked.connect(self.switch_to_login)       # Từ AskTK -> LogIn
+        self.ui.btnLogIn.clicked.connect(self.switch_to_main)     # Từ LogIn -> Main
+        self.ui.btnSignup.clicked.connect(self.switch_to_login)   # Từ SignUp -> LogIn
+
+    def switch_to_askTK(self):
+        """Chuyển từ logo sang màn hình askTK"""
         self.ui.stackedWidget.setCurrentWidget(self.ui.askTK)
-    def switch_login(self):
-        #đăng nhập
+
+    def switch_to_signup(self):
+        """Chuyển từ askTK sang màn hình SignUp"""
+        self.ui.stackedWidget.setCurrentWidget(self.ui.SignUp)
+
+    def switch_to_login(self):
+        """Chuyển từ askTK hoặc SignUp sang màn hình LogIn"""
         self.ui.stackedWidget.setCurrentWidget(self.ui.LogIn)
 
-    def switch_main(self):
-        #vào màn chính
+    def switch_to_main(self):
+        """Chuyển từ LogIn sang màn hình Main"""
         self.ui.stackedWidget.setCurrentWidget(self.ui.Main)
-
-    def switch_signup(self):
-        #đăng kí
-        self.ui.stackedWidget.setCurrentWidget(self.ui.SignUp)
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
